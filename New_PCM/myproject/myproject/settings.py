eaa7f792e2aedebea1ce435b1012782914e5fe72
Ravindra Karale <karaleravi1005@gmail.com>
2018-01-29 23:03:28 +0530

    Update settings.py

--- New_PCM/myproject/myproject/settings.py
+++ New_PCM/myproject/myproject/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = []
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'karaleravi1005@gmail.com'
-EMAIL_HOST_PASSWORD = 'shubhangi1005'
+EMAIL_HOST_PASSWORD = '*********'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
